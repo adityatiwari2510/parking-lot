c59023c096df070e1b4d3697a5a8d6c82a45f1de
Aditya Kedar Tiwari <aditakedart@Adityas-MacBook-Pro.local>
2023-02-17 15:51:24 +0530

    Create: Schema created for parking lot

---
+++ src/main/kotlin/ParkingLot.kt
@@ -0,0 +1,7 @@
+class ParkingLot {
+    private val carParkingSpots = arrayOf<ParkingSpot>()
+    private val entryGate: EntryGate = EntryGate();
+    private val exitGate: ExitGate = ExitGate();
+
+
+}
